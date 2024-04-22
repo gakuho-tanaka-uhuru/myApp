868f6d0a9f35d44c1b0aeb536d515560d8cba0ed
R259\R259-2578 <tanaka.gakuho@uhuru.jp>
2024-04-22 18:02:27 +0900

    firstコミット

---
+++ index.js
@@ -0,0 +1,14 @@
+	
+const express = require("express");
+const app = express();
+
+app.get( '/hello' , function(req,res){
+    // 変数sampleに"こんにちは"という文字列を代入する
+    let sample = "こんにちは"
+    // 変数sampleに代入された値をブラウザに表示する
+    res.send(`<p>TestText${sample}</p>`);
+ });
+ 
+app.listen(3000, function(){
+  console.log("Listening on localhost port 3000");
+});
